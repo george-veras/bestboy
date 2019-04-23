efaf2a5a4460955a95935b227d4e21fc5f907c96
George Véras Valentim <george.veras.valentim@gmail.com>
2019-04-23 11:19:47 -0300

    Starting to break into components.

--- src/components/FileSelector.js
+++ src/components/FileSelector.js
@@ -1,1 +1,20 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+// import 
+
+class FileSelector extends Component {
+  render() {
+    return (
+      <input type="file" onChange={this.handleFileSelection} />
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onChange: () => dispatch(onChange)
+  }
+}
+
+export default FileSelector
