c5e0772290ac41f2f034272f03c0f765364eabe2
George Véras Valentim <george.veras.valentim@gmail.com>
2019-04-25 22:54:38 -0300

    Organizing the first test file.

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,14 +1,28 @@
 'use strict'
 
-import { loadSubtitlesObjs } from './actions'
+import fs from 'fs'
+import path from 'path'
+
+import { loadSubtitleObjs } from './actions'
 
 describe('actions.js', () => {
 
-  before(() => {
-    
-  })
+  describe('.loadSubtitlesObjs(rawText)', () => {
 
-  test('.loadSubtitlesObjs(rawText)', () => {
+    const mockedDispatch = jest.fn()
+    let rawText, dispatchedAction
+
+    beforeAll(() => {
+      const subtitlePath = path.join(__dirname, './example.srt')
+      rawText = fs.readFileSync(subtitlePath, { "encoding": "utf8" })
+      dispatchedAction = loadSubtitleObjs(rawText)(mockedDispatch)
+      console.log(dispatchedAction)
+    })
+
+    test('should load all the', () => {
+      expect(mockedDispatch.mock.calls.length).toBe(1)
+      expect(mockedDispatch.mock.calls[0][0]).toBe({})
+    })
 
   })
 
