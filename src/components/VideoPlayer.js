891c3ca002f953f6132cc7a093cdb6e973d7a12d
George Véras Valentim <george.veras.valentim@gmail.com>
2019-05-02 18:53:42 -0300

    Experimenting with the video component.

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react'
 class VideoPlayer extends Component {
   render() {
     return (
-      <video controls src="/Spider-Man.mp4" autoPlay>
-        <track label="English" kind="subtitles" srcLang="en" src="/Red.Dragon.srt" default />
+      <video controls src="/Spider-Man.mp4" width="800">
+        <track label="English" kind="subtitles" srcLang="en" src="Red.Dragon.srt" default />
+        Your browser doesn't support HTML5 videos.
       </video>
     )
   }
