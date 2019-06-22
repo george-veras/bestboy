833ea11633ef62e29921d7437e3a77a0a030d3af
George Véras Valentim <george.veras.valentim@gmail.com>
2019-06-22 17:16:35 -0300

    Add TitleAndMetaTags component

---
+++ src/components/TitleAndMetaTags.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Helmet from 'react-helmet'
+
+class TitleAndMetaTags extends Component {
+  render() {
+    return (
+      <Helmet></Helmet>
+    )
+  }
+}
+
+export default TitleAndMetaTags
