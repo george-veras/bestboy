db6a0f00810293843d1d03005d69bd2980965bda
George Véras Valentim <george.veras.valentim@gmail.com>
2019-06-22 22:15:20 -0300

    Add Google Analytics logic

--- src/components/TitleAndMetaTags.js
+++ src/components/TitleAndMetaTags.js
@@ -4,7 +4,25 @@ import Helmet from 'react-helmet'
 class TitleAndMetaTags extends Component {
   render() {
     return (
-      <Helmet></Helmet>
+      <Helmet title={this.props.title}>
+        {
+          this.props.gtagId && <script async src={`https://www.googletagmanager.com/gtag/js?id=${this.props.gtagId}`} />
+        }
+        {
+          this.props.gtagId &&
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || []
+              function gtag(){dataLayer.push(arguments)}
+              gtag('js', new Date())
+
+              gtag('config', '${this.props.gtagId}')
+            `}
+          </script>
+        }
+
+        { this.props.canonicalUrl && <link rel="canonical" href={this.props.canonicalUrl}/> }
+      </Helmet>
     )
   }
 }
